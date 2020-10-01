7a567d68f95f60000b49be6e0e0f0fd6fef4e0ec
jitendra shukla <jitendrashukladx@gmail.com>
2020-10-01 20:39:33 +0530

    another prob

---
+++ src/RomanToInteger.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+        public int romanToInt(String s) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            map.put('I', 1);
+            map.put('V', 5);
+            map.put('X', 10);
+            map.put('L', 50);
+            map.put('C', 100);
+            map.put('D', 500);
+            map.put('M', 1000);
+            int add = 0;
+            int roman = 0;
+            for (int i =0; i< s.length(); i++){
+                if(i+1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i+1))){
+                    roman = roman + (map.get(s.charAt(i+1)) - map.get(s.charAt(i)));
+                    i++;
+                } else {
+                    roman += map.get(s.charAt(i));
+                }
+            }
+            return roman;
+        }
+}
